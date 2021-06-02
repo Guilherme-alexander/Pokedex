b2eeca739e3e25df41e46728bb948826a4bbb877
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2021-06-02 20:54:41 -0300

    add Aip

--- js/ajaxJavaScript.js
+++ js/ajaxJavaScript.js
@@ -10,16 +10,18 @@ const generatePokemonPromises = () => Array(493).fill().map((_, index) =>
 
 	Promise.all(pokemonPomises)
 	.then(pokemons => {
-		// console.log(pokemons) debug
+		console.log(pokemons)
 
 		const lisPokemons = pokemons.reduce((accumulator,pokemon) => {
 			const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+			const move = pokemon.moves.map(typeInfo => typeInfo.move.name)
 
 			accumulator += `
 				<li class="card ${types[0]}" tipos2="${types[1]}">
 				<img class="card-imagens ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
 				  <h2 class="card-title ${types[0]}">${pokemon.id}. ${pokemon.name}</h2>
 				  <p class="card-subtitle ${types[0]}">${types.join(' / ')}</p>
+				  <p class="card-move ${types[0]}">${move[0]}</p>
 				</li>
 			` 
 			return accumulator
